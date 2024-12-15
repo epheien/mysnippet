25b82a313919f7d65ddbb93feb1387d1e8fb5ad6
eph <eph@MacBook-Pro.local>
2024-12-15 22:49:27 +0800

    cmake/executable: 添加版本信息

--- cmake/executable/src/main.cpp
+++ cmake/executable/src/main.cpp
@@ -1,6 +1,7 @@
 #include <spdlog/spdlog.h>
 
 #include "main.h"
+#include "version.h"
 
 void HelloWorld() {
     spdlog::info("Hello World");
@@ -8,5 +9,7 @@ void HelloWorld() {
 
 int main(int argc, char* argv[]) {
     HelloWorld();
+
+    spdlog::info("version: {}", VERSION_STRING);
     return 0;
 }
