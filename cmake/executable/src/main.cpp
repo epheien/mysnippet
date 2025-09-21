4fec1f2080268eec83b524113c03f7dbcb713a46
eph <eph@MacBook-Pro.local>
2025-09-22 00:58:02 +0800

    cmake/executable 添加 cli11 库依赖以便于处理命令行参数

--- cmake/executable/src/main.cpp
+++ cmake/executable/src/main.cpp
@@ -1,3 +1,4 @@
+#include <CLI/CLI.hpp>
 #include <spdlog/spdlog.h>
 
 #include "main.h"
@@ -8,6 +9,18 @@ void HelloWorld() {
 }
 
 int main(int argc, char* argv[]) {
+    CLI::App app{argv[0]};
+    bool version = false;
+    std::string config;
+    app.add_flag("-v,--version", version, "print version and exit");
+    app.add_option("-c,--config", config, "config file to read");
+    CLI11_PARSE(app, argc, argv);
+
+    if (version) {
+        fmt::println("{} version: {}", argv[0], VERSION_STRING);
+        return 0;
+    }
+
     HelloWorld();
 
     spdlog::info("version: {}", VERSION_STRING);
