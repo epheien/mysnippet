724ba5215c5a55dfc49c844b4878ac34f7791d93
eph <eph@MacBook-Pro.local>
2025-09-26 03:06:09 +0800

    cmake/executable 完善 cli11 使用示例

--- cmake/executable/src/main.cpp
+++ cmake/executable/src/main.cpp
@@ -1,5 +1,7 @@
 #include <CLI/CLI.hpp>
 #include <spdlog/spdlog.h>
+#include <string>
+#include <vector>
 
 #include "main.h"
 #include "version.h"
@@ -12,8 +14,10 @@ int main(int argc, char* argv[]) {
     CLI::App app{argv[0]};
     bool version = false;
     std::string config;
+    std::vector<std::string> args;
     app.add_flag("-v,--version", version, "print version and exit");
     app.add_option("-c,--config", config, "config file to read");
+    app.add_option("args", args, "args...")->required()->expected(1);
     CLI11_PARSE(app, argc, argv);
 
     if (version) {
@@ -23,6 +27,6 @@ int main(int argc, char* argv[]) {
 
     HelloWorld();
 
-    spdlog::info("version: {}", VERSION_STRING);
+    spdlog::info("version: {}, args count {}", VERSION_STRING, args.size());
     return 0;
 }
